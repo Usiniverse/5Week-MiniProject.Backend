e9d0349976a13ad37e6258318f54e4d345fd6b3c
jinhui_LEE <105095093+Ljinyh@users.noreply.github.com>
2022-06-13 19:15:15 +0900

    like기능 수정
    좋아요 기능 수정

--- controller/likeController.js
+++ controller/likeController.js
@@ -1,44 +1,46 @@
 const Like = require("../models/like");
 
 //좋아요 기능
-async function like (req,res) {
-    const { nickname } = res.locals.user;
-    const { contentId } = req.params;
+async function like(req, res) {
+  const { nickname } = res.locals.user;
+  const { contentId } = req.params;
+  console.log(contentId)
 
-    const findLike = await Like.findOne({nickname});
+  const findLike = await Like.find({ nickname });
+  console.log(findLike)
+    for(let i=0; i<findLike.length; i++){
+        if(contentId === findLike[i].contentId && nickname === findLike[i].nickname){
+            return res.status(400).send({errorMessage: "이미 좋아요를 하셨습니다!"})
+        }}
     
-    if(findLike){
-        res.status(400).send({ errorMessage: "이미 좋아요를 하셨습니다!"});
-    }else{
-
-    const like = await Like.create({
-        nickname, contentId
-    });
-
-
-    res.status(201).json({ result: 'success', msg: "좋아요 완료!"});
-    };
-};
+        const like = await Like.create({
+            nickname,
+            contentId,
+          });
+    
+          return res.status(201).json({ result: "success", msg: "좋아요 완료!" });
+   }
 
 //좋아요 조회
-async function totalLike (req,res) {
-    const findAllLike = await Like.find().count();
-    res.status(200).json(findAllLike);
-};
+async function totalLike(req, res) {
+    const { contentId } = req.params;
+  const findAllLike = await Like.find({contentId}).count();
+  res.status(200).json(findAllLike);
+}
 
 //좋아요 취소
-async function deletelike (req,res) {
-    const { nickname } = res.locals.user;
+async function deletelike(req, res) {
+  const { nickname } = res.locals.user;
 
-    const findLike = await Like.findOne({nickname});
+  const findLike = await Like.findOne({ nickname });
 
-    if(!findLike){
-        res.status(400).send({errorMessage: "좋아요를 하지 않았습니다."});
-    }
+  if (!findLike) {
+    res.status(400).send({ errorMessage: "좋아요를 하지 않았습니다." });
+  }
 
-    const unLike = await Like.findByIdAndDelete(findLike);
-    res.status(200).json({result: 'success', msg: "좋아요 취소 완료!"})
-};
+  const unLike = await Like.findByIdAndDelete(findLike);
+  res.status(200).json({ result: "success", msg: "좋아요 취소 완료!" });
+}
 
 module.exports.like = like;
 module.exports.totalLike = totalLike;
