c12f90c616f2c4ba8e03579c0586f3b061460c05
jinhui_LEE <105095093+Ljinyh@users.noreply.github.com>
2022-06-15 17:12:08 +0900

    수정

--- controller/contentController.js
+++ controller/contentController.js
@@ -5,7 +5,7 @@ const moment = require("moment");
 // 게시글 목록 조회 API
 async function ContentList (req, res) {
     const { page } = req.query;
-    const contentList = await Content.getContentList().skip(page).limit(5);
+    const contentList = await Content.getContentList();
     res.status(200).json( contentList );
 };
 
