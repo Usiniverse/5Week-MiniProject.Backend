5507aecd513084d3d7e10e95cbe2a3e770e0f665
jinhui_LEE <105095093+Ljinyh@users.noreply.github.com>
2022-06-16 02:56:08 +0900

    좋아요 수정

--- controller/likeController.js
+++ controller/likeController.js
@@ -23,7 +23,7 @@ async function like(req, res) {
 //좋아요 조회
 async function totalLike(req, res) {
   const { contentId } = req.params;
-  const findAllLike = await Like.findById({contentId});
+  const findAllLike = await Like.find({contentId});
   res.status(200).json(findAllLike);
 }
 
