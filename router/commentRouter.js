75cef4de9c4c0d84c8652724fdcbc6aa50e3c63d
jinhui_LEE <105095093+Ljinyh@users.noreply.github.com>
2022-06-15 20:51:10 +0900

    test 수정

--- router/commentRouter.js
+++ router/commentRouter.js
@@ -9,7 +9,7 @@ const commentController = require("../controller/commentController");
 router.post("/:contentId", authMiddleware, commentController.postcom);
 
 // get
-router.get("/:contentId", commentController.getcom);
+router.get("/", commentController.getcom);
 
 // patch
 router.patch("/:contentId/:commentId", authMiddleware, commentController.patchcom);
